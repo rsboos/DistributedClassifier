2fe0599a4183e990896eb659b263325d50a7dae7
Bernardo Trevizan <trevizanbernardo@gmail.com>
2017-09-01 15:56:34 -0300

    Class 'Dataset' done

--- src/data.py
+++ src/data.py
@@ -1,3 +1,5 @@
+import math
+import copy
 import numpy
 import pandas
 import sklearn
@@ -7,19 +9,19 @@ class Data():
 	"""Represents the data
 
 	Properties*:
-	data -- instances' attributes
-	target -- instances' classes
-	classes -- list of classes
-
-	*All properties are private
+		data -- instances' attributes
+		target -- instances' classes
+		classes -- list of classes
+		n_columns -- number of columns
+		n_lines -- number of lines
 	"""
 
 	def __init__(self, filepath, class_column=-1):
 		"""Loads the data from a CSV file, normalizes the target and sets the data in the properties
 
 		Keywords arguments:
-		filepath -- file's absolute/relative path
-		class_column -- number of the class column [0 -> first column, (default -1) -> last column]
+			filepath -- file's absolute/relative path
+			class_column -- number of the class column [0 -> first column, (default -1) -> last column]
 		"""
 
 		# Loads the CSV file
@@ -33,8 +35,8 @@ class Data():
 		j = n_columns + class_column	# final + 1 (because it's exclusive)
 
 		# Separates the data from the classes (target)
-		x = dataset.ix[:, i:j]
-		y = dataset.ix[:, class_column]
+		x = dataset.ix[:, i:j]			# all lines and all columns (except the class one)
+		y = dataset.ix[:, class_column] # all lines and the class column
 
 		# Normalizes the target
 		preprocess = sklearn.preprocessing.LabelEncoder() # new LabelEncoder
@@ -50,12 +52,107 @@ class Data():
 		"""Makes a copy of the data and returns it"""
 		return self.__data.copy()
 
+	@data.setter
+	def data(self, other):
+		"""Sets a new value for data
+		
+		Keyword arguments:
+			other -- the new data (must be convertible to ndarray)
+		"""
+
+		self.__data = pandas.DataFrame(__to_ndarray(other))
+
 	@property
 	def target(self):
 		"""Makes a copy of the target and returns it"""
 		return numpy.array(self.__target)
 
+	@target.setter
+	def target(self, other):
+		"""Sets a new value for target
+		
+		Keyword arguments:
+			other -- the new target (must be convertible to ndarray)
+		"""
+
+		self.__target = __to_ndarray(other)
+
 	@property
 	def classes(self):
 		"""Makes a copy of the classes and returns it"""
 		return numpy.array(self.__classes)
+
+	@classes.setter
+	def classes(self, other):
+		"""Sets a new value for classes
+		
+		Keyword arguments:
+			other -- the new classes (must be convertible to ndarray)
+		"""
+
+		self.__classes = __to_ndarray(other)
+
+	@property
+	def n_columns(self):
+		"""Gets the number of columns and returns it"""
+		return self.__data.shape[0] + 1
+
+	@property
+	def n_lines(self):
+		"""Gets the number of lines from data and returns it"""
+		return self.__data.shape[1]
+
+	def __to_ndarray(self, data):
+		"""Converts a list or copy a ndarray e returns the result
+		
+		Keyword arguments:
+			data -- data to be converted (must be convertible to ndarray)
+		"""
+
+		# If the new data is not a convertible to ndarray...
+		if type(data) not in [numpy.ndarray, list]:
+			raise TypeError("%s is not a numpy.ndarray. It must be a list or a ndarray." % type(data))
+
+		return numpy.array(data)
+
+
+class Dataset():
+	"""Prepares the data
+
+	Properties:
+		trainingset -- Data used by the algorithm for training
+		testset -- Data used for testing the model
+	"""
+
+	def __init__(self, filepath, for_test=0.3, class_column=-1):
+		"""Initiates the data and divides into training and testing sets
+
+		Keywords arguments:
+			filepath -- file's absolute/relative path
+			for_test -- percent of test instances (default 0.3)
+			class_column -- number of the class column [0 -> first column, (default -1) -> last column]
+		"""
+
+		# Get the data to split
+		self.trainingset = Data(filepath, class_column)
+
+		# Creates a dataset's copy
+		self.testset = copy.deepcopy(self.trainingset)
+
+		# Calculates the number of instances for the testing set
+		test_ninstances = math.floor(self.trainingset.n_lines * for_test)
+
+		# Split the data
+		data = self.trainingset.data 	 # data from one of the copies
+		target = self.trainingset.target # targets from one of the copies
+
+		self.trainingset.data = data[test_ninstances:, :]	  # gets instances n to len(data)
+		self.trainingset.target = target[test_ninstances:, :] # gets targets n to len(target) 
+
+		self.testset.data = data[:test_ninstances, :]	      # gets instances 0 to n-1
+		self.testset.target = target[:test_ninstances, :]     # gets targets 0 to n-1
+
+
+
+
+
