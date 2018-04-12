77251f2f181f49b9a37863a80dedc06360985b98
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-04-11 21:14:54 -0300

    Print in test and ignore warnings

--- tests.py
+++ tests.py
@@ -1,10 +1,15 @@
+import warnings
+
 from main import main
 from glob import glob
 
+
+warnings.filterwarnings("ignore")
 
 for dataset in glob('datasets/*'):
 
     overlap = float(0)
     while overlap < 1:
+        print('{} {}'.format(overlap, dataset))
         main({'dataset_path': dataset, 'params_path': None, 'overlap': overlap})
         overlap += 0.1
