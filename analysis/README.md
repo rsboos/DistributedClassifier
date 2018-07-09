9cf421cc8d4120c0b0bc766064c4f3fae5efb81b
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-07-09 14:22:35 -0300

    Add command line support

--- analysis/README.md
+++ analysis/README.md
@@ -1,4 +1,23 @@
 # Distributed Classifier
 This application is responsible for analysing evaluation results in `../evaluation/tests/` folder. 
 
-**Note** In development.
+## Command Line
+```
+usage: main.py [-h] [-p PROCESS] [-e EVALUATE] [-t TREES]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p PROCESS, --process PROCESS
+                        Type of process (regression or classification). Create
+                        data sets for evaluation.
+  -e EVALUATE, --evaluate EVALUATE
+                        Type of evaluation (regression or classification).
+                        Evaluate data sets.
+  -t TREES, --make-trees TREES
+                        Only for regression. Create trees from
+                        DecisionTreeRegressor.
+
+```
+
+## Note
+In development.
--- analysis/main.py
+++ analysis/main.py
@@ -1,5 +1,44 @@
-from src.regression import RegressionAnalysis
+import argparse
+from src.regression import RegressionAnalysis, TreeAnalysis
 
-regression = RegressionAnalysis()
-# regression.process('data/datasets.csv', '../evaluation/tests')
-regression.evaluate()
+
+def main(args):
+    regression = 'regression'
+    classificaion = 'classification'
+
+    if args.process == regression:
+        regression = RegressionAnalysis()
+        regression.process('data/datasets.csv', '../evaluation/tests')
+
+    elif args.evaluate == regression:
+        regression = RegressionAnalysis()
+        regression.evaluate()
+
+    elif args.trees == regression:
+        TreeAnalysis.grow_trees()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-p", "--process",
+                        dest="process",
+                        default=None,
+                        help="Type of process (regression or classification). "
+                             "Create data sets for evaluation.")
+
+    parser.add_argument("-e", "--evaluate",
+                        dest="evaluate",
+                        default=None,
+                        help="Type of evaluation (regression or "
+                             "classification). Evaluate data sets.")
+
+    parser.add_argument("-t", "--make-trees",
+                        dest="trees",
+                        default=None,
+                        help="Only for regression. "
+                             "Create trees from DecisionTreeRegressor.")
+
+    args = parser.parse_args()
+    main(args)
