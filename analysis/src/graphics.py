c72ae32a59aa21db0ba014d8c05381acc0eab30b
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-07-09 14:21:51 -0300

    Boxplot graphic

--- analysis/src/graphics.py
+++ analysis/src/graphics.py
@@ -1,51 +1,79 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
+from os import path
 from glob import glob
+from .path import Path
 from pandas import read_csv
 
-classifiers = ['gnb', 'dtree', 'svc', 'knn', 'mlp']
-datasets_folders = glob('tests/*')
-datasets = [d.split('_')[0].replace('tests/', '') for d in datasets_folders]
-rankings = []
 
-for folder in datasets_folders:
-    data = read_csv(folder + '/cv_summary.csv', header=[0, 1], index_col=0)
+class Boxplot:
 
-    # Just mean accuracy
-    try:
-        data = data.loc[:, 'mean'].loc[:, 'f1_macro']
-    except KeyError:
-        data = data.loc[:, 'mean'].loc[:, 'f1']
+    def __init__(self, metric='f1_macro'):
+        self.rankings = []
+        self.ordered_methods = []
+        self.__getter_data(metric)
 
-    data = data.sort_values()
+    def show(self):
+        """Show boxplot."""
+        self.__make()
+        plt.show()
 
-    methods = list(data.index.values)
+    def save(self, filename):
+        """Save figure in Path.graphics_path.
 
-    i = 0
-    while len(methods) < 32:
-        methods.append(classifiers[i])
-        i += 1
+        :param filename: String
+            File name.
 
-    positions = list(enumerate(methods))
+        :return: None
+        """
+        self.__make()
+        plt.savefig(path.join(Path.graphics_path, filename))
 
-    # Sort by name
-    positions.sort(key=lambda x: x[1])
-    ranking, ordered_methods = zip(*positions)
+    def __getter_data(self, metric):
+        metric = [metric, 'f1'] if 'f1_' in metric else [metric, metric]
+        classifiers = ['gnb', 'dtree', 'svc', 'knn', 'mlp']
+        tests_path = '../evaluation/tests/'
 
-    # From 1 to 32
-    ranking = list(map(lambda x: x + 1, ranking))
-    rankings.append(ranking)
+        datasets_folders = glob(path.join(tests_path, '*'))
 
-m, n = len(rankings), len(rankings[0])
-boxplot_data = np.reshape(rankings, (m, n))
+        for folder in datasets_folders:
+            data = read_csv(path.join(folder, Path.default_file),
+                            header=[0, 1], index_col=0)
 
-fig, ax = plt.subplots()
-ax.boxplot(boxplot_data)
+            try:
+                data = data.loc[:, 'mean'].loc[:, metric[0]]
+            except KeyError:
+                data = data.loc[:, 'mean'].loc[:, metric[1]]
 
-ax.set_xlabel('Methods')
-ax.set_ylabel('Rank Position')
-ax.set_xticklabels(ordered_methods, rotation=90)
+            data = data.sort_values()
+            methods = list(data.index.values)
 
-plt.yticks(range(n+1))
-plt.show()
+            i = 0
+            while len(methods) < 32:
+                methods.append(classifiers[i])
+                i += 1
+
+            positions = list(enumerate(methods))
+
+            # Sort by name
+            positions.sort(key=lambda x: x[1])
+            ranking, ordered_methods = zip(*positions)
+
+            # From 1 to 32
+            ranking = list(map(lambda x: x + 1, ranking))
+            self.rankings.append(ranking)
+
+        self.ordered_methods = ordered_methods
+
+    def __make(self):
+        m, n = len(self.rankings), len(self.rankings[0])
+        boxplot_data = np.reshape(self.rankings, (m, n))
+
+        fig, ax = plt.subplots()
+        ax.boxplot(boxplot_data)
+
+        ax.set_xlabel('Methods')
+        ax.set_ylabel('Rank Position')
+        ax.set_xticklabels(self.ordered_methods, rotation=90)
+        plt.yticks(range(n + 1))
