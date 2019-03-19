14d69f3a076ef03cfa685081821c232f496aa799
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-03-19 15:24:26 -0300

    New messages

--- evaluation/evaluate_all.py
+++ evaluation/evaluate_all.py
@@ -2,21 +2,28 @@ import warnings
 
 from main import main
 from glob import glob
-
+from time import time
 
 warnings.filterwarnings("ignore")
-
-tested = [folder.replace('tests/', '') for folder in glob('tests/*')]
-tested = ','.join(tested)
 
 for dataset in glob('datasets/*'):
     dataset_name = dataset.replace('datasets/', '')[:-4]
 
     overlap = float(0)
     while overlap < 1:
+        tested = [folder.replace('tests/', '') for folder in glob('tests/*')]
+        tested = ','.join(tested)
+
         dt_file = dataset_name + '_' + str(int(overlap * 10))
+
         if dt_file not in tested:
+            t = int(time())
+
             print('{} {}'.format(overlap, dataset))
+
             main({'dataset_path': dataset, 'params_path': None, 'overlap': overlap})
+
+            end = (int(time()) - t) // 60
+            print('Completed in {} minutes.'.format(end))
 
         overlap += 0.1
